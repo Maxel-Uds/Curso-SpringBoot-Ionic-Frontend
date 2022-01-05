45d02e0608739c69c04adc8026e54320a1204634
Maxel-Uds <maxellopes32@gmail.com>
2022-01-04 21:57:57 -0300

    Criando a página de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
@@ -13,8 +14,12 @@ export class ProdutoDetailPage {
 
   item: ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public produtoService: ProdutoService, 
+    public cartService: CartService
+  ) {}
 
   ionViewDidLoad() {
     let id = this.navParams.get('produto_id');
@@ -31,6 +36,11 @@ export class ProdutoDetailPage {
       this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
     },
     error => {});
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProdutoToCart(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
---
+++ src/services/domain/cart.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { Cart } from "../../models/cart";
+import { ProdutoDTO } from "../../models/produto.dto";
+import { StorageService } from "../storage.service";
+
+@Injectable()
+export class CartService {
+
+    constructor(public storage: StorageService) {  
+    }
+
+    createOrClearCart() : Cart {
+        let cart: Cart = {items: []};
+        this.storage.setCart(cart);
+
+        return cart; 
+    }
+
+    getCart() : Cart {
+        let cart: Cart = this.storage.getCart();
+        if(cart == null) {
+            cart = this.createOrClearCart();
+        }
+
+        return cart;
+    }
+
+    addProdutoToCart(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(item => item.produto.id == produto.id);
+
+        if(position) {
+            cart.items.push({
+                quantidade: 1, 
+                produto: produto
+            });
+        }
+
+        this.storage.setCart(cart);
+        return cart;
+    }
+}
